17d8e12ad7d88e7e329167dc1279e8a21916a563
gabeschoenbach <51930407+gabeschoenbach@users.noreply.github.com>
2021-07-26 15:43:22 -0400

    adjust pmc chains

---
+++ acceptance.py
@@ -0,0 +1,74 @@
+import math
+from gerrychain import accept
+import numpy as np
+import random
+
+def L1(vec):
+    return sum(abs(v) for v in vec)
+
+def P1(partition, elections):
+    num_dists = len(partition)
+    num_R_seats = 0
+    for election in elections:
+        num_R_seats += partition[election].wins('Rep')
+    average_R_vote_share = np.mean([partition[election].percent('Rep') for election in elections])
+    average_R_seat_share = num_R_seats / (num_dists * len(elections))
+    return abs(average_R_seat_share - average_R_vote_share)
+
+def P2(partition, elections):
+    num_dists = len(partition)
+    proportionality_vector = []
+    for election in elections:
+        vote_share = partition[election].percent("Rep")
+        seat_share = partition[election].wins("Rep") / num_dists
+        proportionality_vector.append(seat_share - vote_share)
+    return L1(proportionality_vector)
+
+def D(partition, elections):
+    num_dists = len(partition)
+    num_R_seats = 0
+    for election in elections:
+        num_R_seats += partition[election].wins('Rep')
+    seat_share = num_R_seats / (num_dists * len(elections))
+    return seat_share
+
+def guided_acceptance_factory(elections, acceptance='D'):
+    
+    def accept_more_P1(partition):
+        parent = partition.parent
+        child_P1 = P1(partition, elections)
+        parent_P1 = P1(parent, elections)
+        probability = min([math.exp(1800*(parent_P1 - child_P1)), 1])
+        if random.random() < probability:
+            return True
+        else:
+            return False
+
+    def accept_more_P2(partition):
+        parent = partition.parent
+        child_P2 = P2(partition, elections)
+        parent_P2 = P2(parent, elections)
+        probability = min([math.exp(180*(parent_P2 - child_P2)), 1])
+        if random.random() < probability:
+            return True
+        else:
+            return False
+        
+    def accept_more_D(partition):
+        parent = partition.parent
+        child_D = D(partition, elections)
+        parent_D = D(parent, elections)
+        probability = min([math.exp(1800*(parent_D - child_D)), 1])
+        if random.random() < probability:
+            return True
+        else:
+            return False
+        
+    if acceptance == 'P1':
+        return accept_more_P1
+    elif acceptance == 'P2':
+        return accept_more_P2
+    elif acceptance == 'D':
+        return accept_more_D
+    else:
+        return accept.always_accept
--- run_pmc_chains.py
+++ run_pmc_chains.py
@@ -13,9 +13,10 @@ import os
 import click
 import random
 from division_aware import *
+from acceptance import *
 
 @click.command()
-@click.option('-epsilon', default=0.01)
+@click.option('-epsilon', default=0.02)
 @click.option('-steps', default=10)
 @click.option('-aware', default=False)
 @click.option('-acceptance', default='D')
@@ -48,26 +49,58 @@ def run_chain(epsilon, steps, aware, acceptance):
                            pop_target=ideal_population,
                            epsilon=epsilon,
                            node_repeats=2)
+
+    elections = ["GOV18", "SEN18", "PRES16", "SEN16", "GOV14", "PRES12", "SEN12", "GOV12"]
+    acceptance_func = guided_acceptance_factory(elections, acceptance)
+    if acceptance == "D":
+        optimizing_func = D
+    elif acceptance == "P1":
+        optimizing_func = P1
+    elif acceptance == "P2":
+        optimizing_func = P2
+    else:
+        optimizing_func = len 
     
     chain = MarkovChain(
                         proposal = proposal,
                         constraints = [constraints.within_percent_of_ideal_population(initial_partition, epsilon)],
-                        accept = accept.always_accept,
+                        accept = acceptance_func,
                         initial_state = initial_partition,
                         total_steps = steps)
 
     split_counties = []
     split_counties = []
     split_munis = []
+    P1s = []
+    P2s = []
+    Ds = []
+    best_score = optimizing_func(initial_partition, elections)
+    best_plan = initial_partition
     for i, part in enumerate(chain):
         split_counties.append(num_division_splits(graph, part, counties, nodes_by_county, division_col="COUNTYFP"))
         split_munis.append(num_division_splits(graph, part, munis, nodes_by_muni, division_col=COUSUB_TYPE))
+        P1s.append(P1(part, elections))
+        P2s.append(P2(part, elections))
+        Ds.append(D(part, elections))
+
+        if acceptance != "always":
+            if optimizing_func(part, elections) <= best_score:
+                best_score = optimizing_func(part, elections)
+                best_plan = part
+        
         if i % 1000 == 0:
             print(i)
     
-    df = pd.DataFrame({'split_counties':split_counties, 'split_munis':split_munis})
+    df = pd.DataFrame({'split_counties':split_counties, 
+                       'split_munis':split_munis,
+                       'P1s':P1s,
+                       'P2s':P2s,
+                       'Ds':Ds})
     print(f"Printing to {run_name}.csv")
+
     df.to_csv(f"outputs/{run_name}.csv", index_label="step")
 
-    if __name__=="__main__":
-        run_chain()
+    save_partition_as_districtr_csv(graph, best_plan, "Code-2", f"{run_name}_best")
+
+if __name__=="__main__":
+    run_chain()
